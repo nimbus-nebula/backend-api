b40f4b9e990fe4143c3dab849c8e936868182fea
Justyn Lantz <jtlantz1995@gmail.com>
2023-03-13 20:56:01 +0700

    add: Some endpoints for authenticating users and tokens

--- app/src/auth/router.py
+++ app/src/auth/router.py
@@ -1,6 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks, Depends, Response, status
 from starlette.requests import Request
 from fastapi_sso.sso.google import GoogleSSO
+
+from src.auth import jwt, service, utils
+from src.auth.dependencies import (
+    valid_refresh_token,
+    valid_refresh_token_user,
+    valid_user_create,
+)
+
+from src.auth.jwt import parse_jwt_user_data
+from src.auth.schemas import (
+    AccessTokenResponse,
+    AuthUser,
+    JWTData,
+    UserResponse
+)
+from databases.interfaces import Record
 
 router = APIRouter()
 google_sso = GoogleSSO("my-client-id",
@@ -25,3 +41,68 @@ async def google_callback(request: Request):
         "email": user.email,
         "provider": user.provider,
     }
+
+
+@router.post("/api/auth/users", status_code=status.HTTP_201_CREATED, response_model=UserResponse)
+async def register_user(
+    auth_data: AuthUser = Depends(valid_user_create),
+) -> dict[str, str]:
+    user = await service.create_user(auth_data)
+    return {
+        "email": user["email"],  # type: ignore
+    }
+
+
+@router.get("/api/auth/users/me", response_model=UserResponse)
+async def get_my_account(
+    jwt_data: JWTData = Depends(parse_jwt_user_data),
+) -> dict[str, str]:
+    user = await service.get_user_by_id(jwt_data.user_id)
+
+    return {
+        "email": user["email"],  # type: ignore
+    }
+
+
+@router.post("/api/auth/users/tokens", response_model=AccessTokenResponse)
+async def auth_user(auth_data: AuthUser, response: Response) -> AccessTokenResponse:
+    user = await service.authenticate_user(auth_data)
+    refresh_token_value = await service.create_refresh_token(user_id=user["id"])
+
+    response.set_cookie(**utils.get_refresh_token_settings(refresh_token_value))
+
+    return AccessTokenResponse(
+        access_token=jwt.create_access_token(user=user),
+        refresh_token=refresh_token_value,
+    )
+
+
+@router.put("/api/auth/users/tokens", response_model=AccessTokenResponse)
+async def refresh_tokens(
+    worker: BackgroundTasks,
+    response: Response,
+    refresh_token: Record = Depends(valid_refresh_token),
+    user: Record = Depends(valid_refresh_token_user),
+) -> AccessTokenResponse:
+    refresh_token_value = await service.create_refresh_token(
+        user_id=refresh_token["user_id"]
+    )
+    response.set_cookie(**utils.get_refresh_token_settings(refresh_token_value))
+
+    worker.add_task(service.expire_refresh_token, refresh_token["uuid"])
+    return AccessTokenResponse(
+        access_token=jwt.create_access_token(user=user),
+        refresh_token=refresh_token_value,
+    )
+
+
+@router.delete("/api/auth/users/tokens")
+async def logout_user(
+    response: Response,
+    refresh_token: Record = Depends(valid_refresh_token),
+) -> None:
+    await service.expire_refresh_token(refresh_token["uuid"])
+
+    response.delete_cookie(
+        **utils.get_refresh_token_settings(refresh_token["refresh_token"], expired=True)
+    )
