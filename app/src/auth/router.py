4501a4a88c0f636c8a131d6f9a09abe00cf06225
Justyn Lantz <jtlantz1995@gmail.com>
2023-03-13 20:46:53 +0700

    change: endpoints in auth to router, change api init to be an api router instead

--- app/src/auth/router.py
+++ app/src/auth/router.py
@@ -1,20 +1,20 @@
-from fastapi import FastAPI
+from fastapi import APIRouter
 from starlette.requests import Request
 from fastapi_sso.sso.google import GoogleSSO
 
-app = FastAPI()
+router = APIRouter()
 google_sso = GoogleSSO("my-client-id",
                        "my-client-secret",
                        "https://my.awesome-web.come/google/callback")
 
 
-@app.get("google/login")
+@router.get("/api/auth/google/login")
 async def google_login():
     """Generate login url and redirect"""
     return await google_sso.get_login_redirect()
 
 
-@app.get("google/callback")
+@router.get("/api/auth/google/callback")
 async def google_callback(request: Request):
     """Handle callback and get user info"""
     user = await google_sso.verify_and_process(request)
